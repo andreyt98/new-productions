ca0c353990c9ec19ef9308a14392672d62c1d6c1
andrey <andreytorrente@gmail.com>
2022-10-22 20:20:02 -0600

    add bootstrap-icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.scss';
 import './main.scss'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
