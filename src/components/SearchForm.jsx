6b7bba63145272a9607463efc370a3f11f63366a
andrey <andreytorrente@gmail.com>
2022-10-22 23:25:15 -0600

    add searchform component

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchForm = () => {
+  return (
+    <form className='searchForm' >
+        <input type="search" placeholder='Search something...' />
+    </form>
+  )
+}
+
+export default SearchForm
