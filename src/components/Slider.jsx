99b99bc9b5d5b31150e543e526d79b54b640fc59
andrey <andreytorrente@gmail.com>
2022-10-25 02:37:43 -0600

    add slider styles and component initial state

---
+++ src/components/Slider.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { imagePoster } from "../helpers/api.config";
+import { fetchData } from "../helpers/fetchData";
+
+const Slider = ({title, mediaType, category,  limit}) => {
+
+  const [results, setResults] = useState([]);
+
+  useEffect(()=>{
+    fetchData({mediaType, category, limit})
+    .then((data)=>{
+      data.forEach(() => {
+        setResults(data)
+      });
+    })
+  },[Slider])
+
+  return (
+    <div className="slider">
+      <div className="slider__header">
+        <h2>{title.toUpperCase()}</h2>
+
+        <div className="controls">
+        <i className="bi bi-chevron-left left"></i>
+        <i className="bi bi-chevron-right right"></i>
+        </div>
+      </div>
+
+      <div className="slider__content">
+        {results.map((element)=>{
+          return(
+            <img src={`${imagePoster}${element.poster_path}`} alt="" />
+          )
+
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
