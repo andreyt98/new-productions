1c0e55ed955be69c5102b5aec9de6ce8c1fa5f9e
andrey <andreytorrente@gmail.com>
2022-10-24 23:26:14 -0600

    add state for hero slider assets

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 
 const HeroSlider = () => {
+  const [heroBackground, setHeroBackground] = useState("");
+  const [results, setResults] = useState([]);
+  const [title, setTitle] = useState("");
+  const [activeImage, setActiveImage] = useState(0);
 
   return (
-    <div className="hero-slider" style={{ backgroundImage: `url(${"heroBackground"})` }}>
+    <div className="hero-slider" style={{ backgroundImage: `url(${heroBackground})` }}>
       <div className="overlay"></div>
       <div className="info">
-        <h1 className="title">{"title"}</h1>
+        <h1 className="title">{title}</h1>
 
         <button> <span><i className="bi bi-play-circle-fill"></i></span> Play</button>
         {/* <p>text</p> */}
