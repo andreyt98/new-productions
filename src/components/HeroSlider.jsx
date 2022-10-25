2484fafc41774b7602b3fa13db30b03b15d615b9
andrey <andreytorrente@gmail.com>
2022-10-24 23:28:45 -0600

    add useEffect to fetch api and change hero slider assets

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import { fetchData} from "../helpers/fetchData";
 
-const HeroSlider = () => {
+const HeroSlider = ({category, mediaType, limit}) => {
   const [heroBackground, setHeroBackground] = useState("");
   const [results, setResults] = useState([]);
   const [title, setTitle] = useState("");
   const [activeImage, setActiveImage] = useState(0);
+
+  useEffect(() => {
+    fetchData({ category, mediaType, limit })
+      .then((data) => {
+        data.forEach(() => {
+          setResults(data);
+          setHeroBackground(window.innerWidth >= 600 ? `${imageWallpaper}${data[0].backdrop_path}` : `${imagePoster}${data[0].poster_path}`);
+          setTitle(data[0].name || data[0].title );
+          setActiveImage(data[0].id)
+        });
+      })
+  }, [HeroSlider]);
 
   return (
     <div className="hero-slider" style={{ backgroundImage: `url(${heroBackground})` }}>
