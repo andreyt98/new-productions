f74a85417347ef55d985030b43124b89df78f9bf
andrey <andreytorrente@gmail.com>
2022-11-13 15:21:05 -0600

    added pages folder and moved search section

---
+++ src/pages/SearchSection/SearchForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { search } from '../../helpers/search'
+
+const SearchForm = ({setResults}) => {
+  function handleSearch(e){
+    e.preventDefault();
+    search(e.target.firstElementChild.value).then((data) => {
+      setResults(data.results);
+    })
+  }
+  return (
+    <form className='searchForm' onSubmit={(e)=> {handleSearch(e)}}>
+        <input type="search" placeholder='Search something...' />
+        <button type="submit">search</button>
+    </form>
+  )
+}
+
+export default SearchForm
---
+++ src/pages/SearchSection/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+import { image } from "../helpers/api.config";
+import SearchForm from "./SearchForm";
+
+const SearchSection = () => {
+  const [results, setResults] = useState([]);
+
+  return (
+    <section className="search-section">
+      <h1>Search for something!</h1>
+      <SearchForm setResults={setResults} />
+      <div className="results">
+        {results.map((element) => {
+          if (element.media_type !== "person") {
+            return (
+              <div className="content">
+                <div className="overlay">
+                  <i
+                    className="bi bi-play-circle-fill"
+                    data-id={element.id}
+                    onClick={() => {
+                      handleTrailerClick(element.id);
+                    }}
+                  ></i>{" "}
+                </div>
+                <img src={`${image({ size: 500 })}${element.poster_path}`} alt="" />
+              </div>
+            );
+          }
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default SearchSection;
