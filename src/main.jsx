699c8dcebb8486df930f4919ef5277ebbf1b5adc
andrey <andreytorrente@gmail.com>
2022-11-13 15:14:30 -0600

    move sass main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.scss';
-import './main.scss'
+import './sass/main.scss'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
