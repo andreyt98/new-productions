3792ded4b4988f785dd433e251020ccff4871bc9
andrey <andreytorrente@gmail.com>
2022-11-17 20:34:24 -0600

    add button and state to minimize trailer

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import PortalReactDom from "react-dom";
 import ReactPlayer from "react-player";
 
 const Trailer = ({ openTrailer, setOpenTrailer, trailerKey }) => {
   if (!openTrailer) return null;
 
+  const [min,setMin] = useState(false);
+  const [minText,setMinText] = useState("Minimize");
+  
+  function handleMin(){
+    setMin(!min)
+
+    if(min){
+      setMinText("Minimize")
+    }else{
+      setMinText("Maximize")
+    }
+  }
+
   return PortalReactDom.createPortal(
-    <div className="trailer">
-      <button className="close-btn" onClick={() => {setOpenTrailer(false)}}>Close X</button>      
+    <div className={min? 'min' : 'trailer'}>
+      <button className="trailer-btn close-btn" onClick={() => {setOpenTrailer(false)}}>Close X</button>      
+      <button className="trailer-btn min-btn" onClick={() => {handleMin()}}>{minText}</button>      
       <ReactPlayer url={`https://www.youtube.com/watch?v=${trailerKey}`}     
       width="100%"
       height="100%"
