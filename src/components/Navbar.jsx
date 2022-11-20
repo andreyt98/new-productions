8f45ef44cba6f5b4eda4cbebad6c7cdb914a6d66
andrey <andreytorrente@gmail.com>
2022-11-19 19:47:37 -0600

    make small size navbar always visible

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,17 @@
-import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const [navBar, setNavBar] = useState(false);
-
-  const navBarState = navBar ? "links-active" : "links-inactive";
 
   return (
     <nav className="nav">
       <Link to="/" end><i className="bi bi-bullseye" id="logo"></i></Link>
 
-      <ul className={`${navBarState} links`}>
-        <li><NavLink to="tvshows" onClick={() => {setNavBar(false)}}>TV Shows</NavLink></li>
-        <li><NavLink to="movies" onClick={() => {setNavBar(false)}}>Movies</NavLink></li>
-      </ul>
-
       <NavLink to="search" id="search-btn"><i className="bi bi-search"></i></NavLink>
-
-      <i className="bi bi-list" id="menu-icon" onClick={() => {setNavBar(!navBar)}}></i>
+      <ul className={` links`}>
+        <li><NavLink to="movies"> Movies</NavLink></li>
+        <li><NavLink to="tvshows"> TV Shows</NavLink></li>
+        <li><NavLink to="Saved">  Favorites</NavLink></li>
+      </ul>
     </nav>
   );
 };
