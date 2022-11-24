d791494249ac239023cbf9b21d3e091272ea3df1
andrey <andreytorrente@gmail.com>
2022-11-24 13:36:34 -0600

    add change of navbar style when scrolling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
+import { useRef } from "react";
+import { useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const navRef = useRef();
+
+  useEffect(() => {
+   window.addEventListener("scroll", ()=> {
+
+    if(window.scrollY>0){
+      navRef.current.style.backgroundColor = "#0007"
+      navRef.current.style.backdropFilter = "blur(8px)"
+    }else{
+      navRef.current.style.background = "none"
+      navRef.current.style.backdropFilter = "none"
+    }
+   })
+  }, [])
+  
 
   return (
-    <nav className="nav">
+    <nav className="nav" ref={navRef}>
       <Link to="/"><i className="bi bi-bullseye" id="logo"></i></Link>
 
       <NavLink to="search" id="search-btn"><i className="bi bi-search"></i></NavLink>
--- src/helpers/getMovies.js
+++
@@ -1,9 +0,0 @@
-import { apiUrl,API_KEY } from "./api.config"
-
-export const getMovies = async (id, mediaType) => {
-
-   const data =  await fetch(`${apiUrl}${mediaType}/${id}/videos?api_key=${API_KEY}`);
-    const json = await data.json();
-
-    return json;
-}
