49b39d48b829ccbdf9f8148a3eee043d89fdfeeb
andrey <andreytorrente@gmail.com>
2022-11-26 15:55:18 -0600

    add context values to app component

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,36 @@ import MovieSection from './components/MovieSection';
 import Navbar from './components/Navbar';
 import SearchSection from './pages/SearchSection/index';
 import TvSection from './components/tvSection';
+import Info from './components/Info';
+import { useState } from 'react';
+import { Context } from './context/Context';
 
 function App() {
+  const [currentId, setCurrentId] = useState();
+
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Navbar />
+      <Context.Provider
+        value={{
+          currentId,
+          setCurrentId,
+        }}
+      >
+        <BrowserRouter>
+          <Navbar />
 
-        <Routes>
-          <Route path='/movies' element={<MovieSection />}></Route>
-          <Route path='/tvshows' element={<TvSection />}></Route>
-          <Route path='/search' element={<SearchSection />}></Route>
-          <Route path='*' element={<Navigate to='/movies' />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path='/movies' element={<MovieSection />}></Route>
+            <Route path='/tvshows' element={<TvSection />}></Route>
+            <Route path='/search' element={<SearchSection />}></Route>
+
+            <Route path='/movies/:id' element={<Info mediaType={'movie'} />}></Route>
+            <Route path='/tvshows/:id' element={<Info mediaType={'tv'} />}></Route>
+
+            <Route path='*' element={<Navigate to='/movies' />} />
+          </Routes>
+        </BrowserRouter>
+      </Context.Provider>
     </div>
   );
 }
