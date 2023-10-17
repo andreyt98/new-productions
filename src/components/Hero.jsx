a51836add936f0de206385a3c84f9677bd7ad197
Andrey <devandrey98@gmail.com>
2023-10-16 20:33:25 -0600

    feat:abstracted functionality of setting the provider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,39 +29,8 @@ const Hero = ({ mediaType, category, limit }) => {
           setTitle(firstResult.name || firstResult.title);
         });
     
-        getProvider(firstResult.id, mediaType).then((providerData) => {
-          const { results: providerResults } = providerData;
-          const noProvider = Object.keys(providerData.results).length < 1;
-          if (noProvider) {
-            setProvider('');
-            return;
-          }
-    
-          if (providerResults) {
-            Object.values(providerResults).map((result) => {
-              if (providerResults.US != undefined) {
-                if (providerResults.US.flatrate != undefined) {
-                  Object.values(providerResults.US.flatrate).map((flatrateResult, index) => {
-                    if (index === 0) {
-                      setProvider(flatrateResult.provider_name);
-                    }
-                  });
-                } else {
-                  Object.values(providerResults.US)[1].map((AnyResult, index) => {
-                    if (index === 0) {
-                      setProvider(AnyResult.provider_name);
-                    }
-                  });
-                }
-              } else {
-                Object.values(result)[1].map((fromAnyLanguage) => {
-                  setProvider(Object.values(fromAnyLanguage)[2]);
-                });
-              }
-            });
-          }
-        });
-    
+        assignProvider(firstResult)
+        
         setId(firstResult.id);
       }else{
         throw new Error
@@ -80,44 +49,58 @@ const Hero = ({ mediaType, category, limit }) => {
   function handleImageClick(event, result) {
     setId(result.id);
 
+    const clickedElement = event.target.dataset.id
+    
     results.forEach((element) => {
-      if (event.target.dataset.id == element.id) {
+
+      const elementExistInApi = clickedElement == element.id
+
+      if (elementExistInApi) {
         setHeroBackground(window.innerWidth >= 640 ? `${image({ size: 1280 })}${element.backdrop_path}` : `${image({ size: 500 })}${element.poster_path}`);
         setTitle(element.name || element.title);
-
-        getProvider(element.id, mediaType).then((providerData) => {
-          const noProvider = Object.keys(providerData.results).length < 1;
-          if (noProvider) {
-            setProvider('');
-            return;
-          }
-          const { results } = providerData;
-
-          if (results) {
-            Object.values(results).map((result) => {
-              if (results.US != undefined) {
-                if (results.US.flatrate != undefined) {
-                  Object.values(results.US.flatrate).map((flatrateResult, index) => {
-                    if (index === 0) {
-                      setProvider(flatrateResult.provider_name);
-                    }
-                  });
-                } else {
-                  Object.values(results.US)[1].map((AnyResult, index) => {
-                    if (index === 0) {
-                      setProvider(AnyResult.provider_name);
-                    }
-                  });
-                }
-              } else {
-                Object.values(result)[1].map((fromAnyLanguage) => {
-                  setProvider(Object.values(fromAnyLanguage)[2]);
-                });
-              }
-            });
-          }
-        });
+        assignProvider(element)
       }
+    });
+  }
+
+
+const assignProvider = (element) =>{
+  getProvider(element.id, mediaType).then((providerData) => {
+    const noProvider = Object.keys(providerData.results).length < 1;
+    if (noProvider) {
+      setProvider('');
+      return;
+    }
+    const { results } = providerData;
+
+    if (results) {
+      Object.values(results).map((result) => {
+        results.US? setProviderFromUsa(results) : setProviderFromAnyCountry(result)       
+      });
+    }
+  });
+}
+
+  const setProviderFromUsa = (results)=> {
+
+    if (results.US.flatrate) {
+      Object.values(results.US.flatrate).map((flatrateResult, index) => {
+        if (index === 0) {
+          setProvider(flatrateResult.provider_name);
+        }
+      });
+    } else {
+      Object.values(results.US)[1].map((AnyResult, index) => {
+        if (index === 0) {
+          setProvider(AnyResult.provider_name);
+        }
+      });
+    }
+  }
+
+  const setProviderFromAnyCountry = (result)=> {
+    Object.values(result)[1].map((fromAnyLanguage) => {
+      setProvider(Object.values(fromAnyLanguage)[2]);
     });
   }
 
