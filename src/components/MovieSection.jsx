a7dc50d58f19699348502f2c8c64791224d27dad
Andrey <devandrey98@gmail.com>
2023-11-05 14:16:53 -0600

    refactor: deleted props from each section

--- src/components/MovieSection.jsx
+++ src/components/MovieSection.jsx
@@ -1,14 +1,13 @@
-import Hero from './Hero'
-import Slider from './Slider'
+import Hero from './Hero';
+import Slider from './Slider';
 
 const MovieSection = () => {
-
   return (
     <section>
-        <Hero category={"trending"} mediaType={"movie"} limit={4}/> 
-        <Slider category={"popular"} mediaType={"movie"} limit={7}/>
+      <Hero />
+      <Slider />
     </section>
-  )
-}
+  );
+};
 
-export default MovieSection
+export default MovieSection;
--- src/components/TvSection.jsx
+++ src/components/TvSection.jsx
@@ -1,12 +1,11 @@
-import Hero from "./Hero";
-import Slider from "./Slider";
+import Hero from './Hero';
+import Slider from './Slider';
 
 const TvSection = () => {
-
   return (
-    <section className="tv-section">
-      <Hero category={"trending"} mediaType={"tv"} limit={4} />
-      <Slider category={"popular"} mediaType={"tv"} limit={7} />
+    <section className='tv-section'>
+      <Hero />
+      <Slider />
     </section>
   );
 };
