cdde3c685d8d165907c27bc3355d6cc931558fa7
Andrey <devandrey98@gmail.com>
2023-11-05 14:19:26 -0600

    feat: added state to save the current media type and to save the api data retrieved from api

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ function App() {
   const [currentId, setCurrentId] = useState();
   const [openTrailer, setOpenTrailer] = useState('');
   const [trailerKey, setTrailerKey] = useState('');
-  
+  const [currentMediaType, setCurrentMediaType] = useState(window.location.pathname.slice(1));
+  const [apiData, setApiData] = useState([])
+
   return (
-    <Context.Provider value={{ currentId, setCurrentId, openTrailer, setOpenTrailer,trailerKey, setTrailerKey}}>
+
+    <Context.Provider value={{ currentId, setCurrentId, openTrailer, setOpenTrailer,trailerKey, setTrailerKey,
+    currentMediaType,setCurrentMediaType,apiData, setApiData}}>
       <div className='App'>
         <AppRouter />
       </div>
