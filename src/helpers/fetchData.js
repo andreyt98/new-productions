7df1ad9a1e23a6c327f2a15ee59b5a98ca86a589
Andrey <devandrey98@gmail.com>
2023-11-05 14:28:28 -0600

    feat: changed fetchData function to make 2 requests at a time and to return an empty array if something fails

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,16 +1,28 @@
 import { apiUrl, API_KEY } from "./api.config";
 
-export const fetchData = async ({ mediaType, category, limit }) => {
-  const path = category === "trending" ? `${category}/${mediaType}/day` : `${mediaType}/${category}/`;
-  const data = await fetch(`${apiUrl}${path}?api_key=${API_KEY}&page=1`);
-  const json = await data.json();
+export const fetchData = async (obj) => {
 
-  if (data.ok) {
-    if (limit === undefined) limit = json.results.length;
-    
-    return json.results.slice(0, limit);
+  const {mediaType, category, limit  } = obj;
+  const path1 = `${category[0]}/${mediaType}/day`;
+  const path2 = `${mediaType}/${category[1]}`;
 
-  } else {
+  const trendingData = await fetch(`${apiUrl}${path1}?api_key=${API_KEY}&page=1`);
+  const popularData = await fetch(`${apiUrl}${path2}?api_key=${API_KEY}&page=1`);
+
+  const json1 = await trendingData.json();
+  const json2 = await popularData.json();
+
+  if(trendingData.ok && popularData.ok){
+
+    const jsonTrendingData = json1.results.slice(0, limit[0])
+    const jsonPopularData = json2.results.slice(0, limit[1])
+
+    const dataArray = []
+    dataArray.push(jsonTrendingData, jsonPopularData)
+
+    return dataArray
+
+  }else{
     return []; 
   }
 };
