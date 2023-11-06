b8417f54cecf060b82214d15566c5c4bbd450b5a
Andrey <devandrey98@gmail.com>
2023-11-05 22:05:40 -0600

    Moved functions related top provider to getProvider file to reuse them

--- src/helpers/getProviders.js
+++ src/helpers/getProviders.js
@@ -6,3 +6,35 @@ export const getProvider = async (id, mediaType) => {
 
   return json;
 };
+
+export const assignProvider = async (id,currentMediaType,setProvider) => {
+  try {
+    const mediaType = currentMediaType == 'movies' ? 'movie' : 'tv'
+    
+    const providerResults = await getProvider(id, mediaType);
+
+    const noProvider = Object.keys(providerResults.results).length < 1;
+    if (noProvider) {
+      setProvider('');
+      return;
+    }
+
+    const { results } = providerResults;
+
+    if (results) {
+      Object.values(results).map((result) => {
+        results.US ? setProviderFromUsa(results,setProvider) : setProvider(Object.values(result)[1][0].provider_name);
+      });
+    }
+  } catch(e) {
+    console.log('error on provider', e);
+  }
+};
+
+const setProviderFromUsa = (results,setProvider) => {
+  if (results.US.flatrate) {
+    setProvider(Object.values(results.US.flatrate[0].provider_name));
+  } else {
+    setProvider(Object.values(results.US)[1][0].provider_name);
+  }
+};
