db4c6d06b30a82c7c060b10f233e4f9ecc7cac84
Andrey <devandrey98@gmail.com>
2024-02-27 23:10:47 -0600

    feat: added cast content, its styles and added function that fetch data to this component

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ function App() {
   const [currentId, setCurrentId] = useState();
   const [openTrailer, setOpenTrailer] = useState('');
   const [trailerKey, setTrailerKey] = useState('');
+  const [cast, setCast] = useState('')
   const [currentMediaType, setCurrentMediaType] = useState(window.location.pathname.slice(1));
   const [apiData, setApiData] = useState([])
 
   return (
 
     <Context.Provider value={{ currentId, setCurrentId, openTrailer, setOpenTrailer,trailerKey, setTrailerKey,
-    currentMediaType,setCurrentMediaType,apiData, setApiData}}>
+    currentMediaType,setCurrentMediaType,apiData, setApiData, cast, setCast}}>
       <div className='App'>
         <AppRouter />
       </div>
