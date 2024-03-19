546057e189c09e9a168b9f0e1f94685c82d9b406
Andrey <devandrey98@gmail.com>
2024-03-19 01:04:08 -0600

    refactor: created object to used as value of context to improve readability

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,23 @@ function App() {
   const [currentMediaType, setCurrentMediaType] = useState(window.location.pathname.slice(1));
   const [apiData, setApiData] = useState([])
 
-  return (
+  const contextValues = { 
+      currentId,
+      setCurrentId,
+      openTrailer,
+      setOpenTrailer,
+      trailerKey,
+      setTrailerKey,
+      currentMediaType,
+      setCurrentMediaType,
+      apiData,
+      setApiData,
+      cast,
+      setCast
+  }
 
-    <Context.Provider value={{ currentId, setCurrentId, openTrailer, setOpenTrailer,trailerKey, setTrailerKey,
-    currentMediaType,setCurrentMediaType,apiData, setApiData, cast, setCast}}>
+  return (
+    <Context.Provider value={contextValues}>
       <div className='App'>
         <AppRouter />
       </div>
