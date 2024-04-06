ecb36bac1ef40b25aa3255a9686a51c4c72f6522
Andrey <devandrey98@gmail.com>
2024-04-05 23:53:05 -0600

    fix: added prop to slidercard component to change current mediatype when clicking content to get correct result info

--- src/pages/SearchSection/index.jsx
+++ src/pages/SearchSection/index.jsx
@@ -7,19 +7,19 @@ import SearchForm from './SearchForm';
 
 const SearchSection = () => {
   const [results, setResults] = useState([]);
-  const {openTrailer} = useContext(Context)
+  const { openTrailer } = useContext(Context);
 
   return (
     <section className={`search-section ${openTrailer && 'on-trailer'}`}>
       <h1>Search for something!</h1>
       <SearchForm setResults={setResults} />
       <div className='results'>
-        {results==null ? (
+        {results == null ? (
           <img src='../../../public/spinner.gif' alt='' className='spinner' />
         ) : (
           results.map((result) => {
-            if (result.media_type !== 'person') {
-              return <SliderCard result={result} />;
+            if (result.media_type !== 'person' && result.media_type) {
+              return <SliderCard result={result} changeMediaType={result.media_type} />;
             }
           })
         )}
