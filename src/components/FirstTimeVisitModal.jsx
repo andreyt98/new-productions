345b0dcdfd6f85d5ff424a319be18544b6502b4a
Andrey <devandrey98@gmail.com>
2024-05-21 13:08:34 -0600

    refact: deleted button to close modal and update text

--- src/components/FirstTimeVisitModal.jsx
+++ src/components/FirstTimeVisitModal.jsx
@@ -1,8 +1,6 @@
 import { useContext } from 'react';
 import { Context } from '../context/Context';
-import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -11,50 +9,51 @@ import { Link } from 'react-router-dom';
 const FirstTimeVisitModal = () => {
   const { isMember, openDialog, setOpenDialog, setUserClicked } = useContext(Context);
 
-  return !isMember &&
-    <Dialog
-      open={openDialog}
-      onClose={() => {
-        setOpenDialog(false);
-      }}
-      aria-labelledby='responsive-dialog-title'
-    >
-      <DialogTitle style={{ textShadow: 'none' }} id='responsive-dialog-title'>
-        {'Sign up for free '}
+  return (
+    !isMember && (
+      <Dialog
+        open={openDialog}
+        onClose={() => {
+          setOpenDialog(false);
+        }}
+        aria-labelledby='responsive-dialog-title'
+      >
+        <DialogTitle style={{ textShadow: 'none' }} id='responsive-dialog-title'>
+          {`There's more! `}
 
-        <img style={{ width: '20px' }} src='https://em-content.zobj.net/source/apple/391/grinning-face_1f600.png' alt='' />
-      </DialogTitle>
-      <DialogContent style={{ textShadow: 'none' }}>
-        <DialogContentText style={{ background: '' }}>
-          This app works better if you have an account. Consider
-          <Link
-            to='#'
-            onClick={() => {
-              setUserClicked(true);
-              setOpenDialog(false);
-            }}
-            style={{ color: '#00a87e' }}
-          >
-            {' '}
-            signing up{' '}
-          </Link>{' '}
-          to access all functionalities.
-          <p style={{ fontSize: '70%', marginTop: '10px', fontStyle: 'italic' }}>this message will be displayed just once.</p>
-        </DialogContentText>
-      </DialogContent>
-
-      <DialogActions style={{ background: '' }}>
-        <Button
-          style={{ backgroundColor: '#2727271c', fontSize: '70%', padding: '1px 7px', color: '#4e4e4e', borderRadius: '25px' }}
-          className='button'
-          onClick={() => {
-            setOpenDialog(false);
-          }}
-        >
-          close
-        </Button>
-      </DialogActions>
-    </Dialog>
+          <img style={{ width: '20px' }} src='https://em-content.zobj.net/source/apple/391/grinning-face_1f600.png' alt='' />
+        </DialogTitle>
+        <DialogContent style={{ textShadow: 'none' }}>
+          <DialogContentText style={{ background: '' }}>
+            Consider{' '}
+            <Link
+              to='#'
+              onClick={() => {
+                setUserClicked(true);
+                setOpenDialog(false);
+              }}
+              style={{ color: '#00a87e', textDecoration: 'underline' }}
+            >
+              signing up
+            </Link>{' '}
+            for free to access all functionalities.
+            <p style={{ fontSize: '80%', marginTop: '10px', fontStyle: 'italic' }}>
+              Or{' '}
+              <span
+                onClick={() => {
+                  setOpenDialog(false);
+                }}
+                style={{ cursor: 'pointer', fontStyle: 'underline', textDecoration: 'underline' }}
+              >
+                close this message
+              </span>{' '}
+              to keep navigating.
+            </p>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    )
+  );
 };
 
 export default FirstTimeVisitModal;
