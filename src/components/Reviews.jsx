3822ed0a0c72fd9a4fa9dfe54d13b70dc7ebfe28
Andrey <devandrey98@gmail.com>
2024-06-13 23:31:57 -0600

    feat: added component for reviews content and added logic to show full review

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,54 +1,17 @@
-import { useState, useRef } from 'react';
-
+import { useRef } from 'react';
+import ReviewCard from './ReviewCard';
 export const Reviews = ({ reviews }) => {
-
   const reviewsContainerRef = useRef(null);
-  const [reviewsMaximized, setReviewsMaximized] = useState(false);
+
   return (
     <>
       <h3 style={{ marginTop: '40px' }}>Reviews</h3>
       {reviews.length > 0 ? (
-        <>
-          <div className='reviews' ref={reviewsContainerRef} style={{ height: reviews.length < 2 ? '100%' : '350px', position: 'relative', zIndex: '1' }}>
-            {reviews.map((result) => {
-              return (
-                <div className='review-content'>
-                  <span className='fixed' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <p id='author'>
-                      {result.author_details.username}
-                      <p style={{ color: 'white', opacity: '0.7', fontSize: '85%' }}>Rating: {result.author_details.rating}</p>
-                    </p>
-                    <p style={{ opacity: '0.7' }}>{result.created_at.slice(0, 10)}</p>
-                  </span>
-
-                  <p className='review-text'>{result.content}</p>
-                </div>
-              );
-            })}
-
-            <span
-              style={{
-                display: reviews.length < 2 || reviewsMaximized ? 'none' : 'block',
-                zIndex: '2',
-                height: '300px',
-                width: '100%',
-                position: 'absolute',
-                bottom: '0',
-                left: '0',
-                background: reviews.length > 1 ? 'linear-gradient(transparent, #000000de, black)' : 'none',
-              }}
-            >
-              <p
-                onClick={() => {
-                  (reviewsContainerRef.current.style.height = '100%'), setReviewsMaximized(true);
-                }}
-                style={{ textDecoration: 'underline', cursor: 'pointer', textAlign: 'center', position: 'absolute', bottom: '0', left: '50%', transform: 'translate(-50%)' }}
-              >
-                See all
-              </p>
-            </span>
-          </div>
-        </>
+        <div className='reviews' ref={reviewsContainerRef}>
+          {reviews.map((result) => {
+            return <ReviewCard result={result} />;
+          })}
+        </div>
       ) : (
         <p style={{ textAlign: 'center' }}>No reviews available</p>
       )}
