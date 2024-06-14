6c262e56d71283759792eb73a7b8a09b44e2c62c
Andrey <devandrey98@gmail.com>
2024-06-13 18:31:53 -0600

    added navigation

--- src/pages/SearchSection/index.jsx
+++ src/pages/SearchSection/index.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { useContext } from 'react';
-import { useState } from 'react';
+import { useContext,useState } from 'react';
 import SliderCard from '../../components/SliderCard';
 import { Context } from '../../context/Context';
 import CircularProgress from '@mui/material/CircularProgress';
 import { search } from '../../helpers/search';
+import { useNavigate } from 'react-router-dom';
 
 const SearchSection = () => {
   const { openTrailer, searchResults, setSearchResults } = useContext(Context);
   const [loadingSearch, setLoadingSearch] = useState(false);
   const [searchStarted, setSearchStarted] = useState(false);
+  const navigate = useNavigate();
   const [inputValue, setInputValue] = useState('');
 
   function handleSearch(event) {
     event.preventDefault();
-    setSearchResults([]);
-    setSearchStarted(true);
-
     if (inputValue.trim().length === 0) {
       return;
     }
+    setSearchResults([]);
+    setSearchStarted(true);
+
     setLoadingSearch(true);
 
     search(inputValue).then((data) => {
@@ -30,41 +30,45 @@ const SearchSection = () => {
 
   return (
     <section className={`search-section ${openTrailer && 'on-trailer'}`}>
-      <form
-        className='searchForm'
-        onSubmit={(event) => {
-          handleSearch(event);
-        }}
-      >
-        <input
-          type='search'
-          placeholder='Search for a movie or tv show...'
-          value={inputValue}
-          onChange={(event) => {
-            setInputValue(event.target.value);
+      <div className='form-bg'>
+        <i
+          className='bi bi-arrow-left'
+          onClick={() => {
+            navigate(-1);
+            window.scrollTo(0, 0);
           }}
-        />
-        <button type='submit'>
-          <i className='bi bi-search'></i>
-        </button>
-      </form>
+        ></i>
+        <form
+          className='searchForm'
+          onSubmit={(event) => {
+            handleSearch(event);
+          }}
+        >
+          <input
+            type='search'
+            placeholder='Search for a movie or tv show...'
+            value={inputValue}
+            onChange={(event) => {
+              setInputValue(event.target.value);
+            }}
+          />
+          <button type='submit'>
+            <i className='bi bi-search'></i>
+          </button>
+        </form>
+      </div>
       {loadingSearch ? (
         <CircularProgress color='inherit' size={100} style={{ marginTop: '100px' }} />
       ) : (
         <>
           <div className='results'>
-            {searchStarted ? (
-              searchResults.length > 0 ? (
-                searchResults.map((result) => {
+            {searchResults.length > 0
+              ? searchResults.map((result) => {
                   if (result.media_type !== 'person' && result.media_type) {
-                    return <SliderCard result={result} changeMediaType={result.media_type} key={result.id} />;
+                    return <SliderCard result={result} changeMediaType={result.media_type} />;
                   }
-                  return null;
                 })
-              ) : (
-                <p style={{ gridColumn: '1/-1' }}>no results</p>
-              )
-            ) : null}
+              : searchStarted && <p style={{ gridColumn: '1/-1' }}>no results</p>}
           </div>
         </>
       )}
